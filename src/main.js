f5bcac901953721d0e502b347df98041e674e6d7
HyeonuPark <nemo1275@gmail.com>
2015-06-25 10:09:22 +0900

    initial release

---
+++ src/main.js
@@ -0,0 +1,23 @@
+function PromiseEmitter() {
+    this.chain = [];
+}
+
+PromiseEmitter.prototype.then = function then(resolve, reject) {
+    this.chain.push({
+        resolve : resolve,
+        reject : reject
+    });
+    return this;
+};
+
+PromiseEmitter.prototype.catch = function(reject) {
+    return this.then(null, reject);
+};
+
+PromiseEmitter.prototype.emit = function emit(event) {
+    var promise = Promise.resolve(event);
+    this.chain.forEach(function(each) {
+        promise = promise.then(each.resolve, each.reject);
+    });
+    return promise;
+};
